43934362f9a4bafdfe48b149228e086a1a66beee
mukund kumar <mukund5457@gmail.com>
2024-02-13 02:13:10 +0530

    setting up project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+import logo from '../../images/logo.png'
+function Navbar() {
+
+    return(
+        
+        <div>
+            <img src={logo} alt='logo'/>
+        </div>
+        
+    )
+}
+export default Navbar;
